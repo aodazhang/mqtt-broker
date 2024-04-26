b7f9189e322101a06a6656ac89cd14b2e3e4985c
aodazhang <aodazhang@qq.com>
2024-04-26 16:20:12 +0800

    refactor(src): 重构客户端示例 demo

--- README.md
+++ README.md
@@ -1,3 +1,38 @@
 # mqtt-broker
 
-个人服务器使用 mqtt broker
+## 一.简介
+
+本项目为个人服务器部署的 mqtt broker，访问地址如下：
+
+- mqtt 协议：mqtt://106.15.185.226:1883
+- websocket 协议：ws://106.15.185.226:8083/mqtt
+
+该 broker 基于 [aedes](https://github.com/moscajs/aedes) 开发，前端推荐 [mqtt.js](https://github.com/mqttjs/MQTT.js) 作为 mqtt 客户端。
+
+## 二.脚本指令
+
+- **`npm start`**：启动 mqtt broker
+- **`npm run start:ws`**：启动 mqtt broker（websocket 协议）
+- **`npm run start:client`**：启动开发服务器 http://localhost:8080/client.html
+- **`npm run start:pm2`**：通过 pm2 启动生产环境服务器（该指令仅执行一次，多次运行会导致 pm2 文件监听无效）
+- **`npm run lint`**：prettier 格式化代码 + eslint 检查代码
+- **`npm run commit`**：commitizen 提交代码到本地仓库
+- **`npm run prepare`**：husky 初始化运行脚本
+
+## 三.日志
+
+目录结构如下：
+
+```
+logs
+├── mqtt_YYYY-MM-DD.log # mqtt 协议日志（按日拆分）
+├── ...
+├── mqtt_ws_YYYY-MM-DD.log # websocket 协议日志（按日拆分）
+└── ...
+```
+
+日志格式如下：
+
+```
+[时间戳] [类型] <服务端 id | 客户端 id | 当前客户端链接数> 内容
+```
---
+++ src/client.js
@@ -0,0 +1,169 @@
+import mqtt from 'mqtt'
+
+/**
+ * 创建 mqtt 服务
+ * - 开发：ws://localhost:8083/mqtt
+ * - 生产：ws://106.15.185.226:8083/mqtt
+ */
+const client = mqtt.connect('ws://106.15.185.226:8083/mqtt', {
+  username: 'admin', // broker 用户名
+  password: '123456', // broker 密码
+  connectTimeout: 10 * 1000, // 连接超时：30-30s
+  keepalive: 60, // 单次保持连接时长：0-禁用、60-60s
+  reconnectPeriod: 10 * 1000 // 重连间隔：0-禁用、1000-1000ms
+})
+
+// [事件]首次、重新连接成功
+client.on('connect', packet => {
+  /**
+   * packet：数据包
+   */
+  console.log('连接成功', {
+    time: new Date().toLocaleString(),
+    客户端id: client.options.clientId,
+    是否连接服务端: client.connected,
+    是否重连服务端: client.reconnecting,
+    client,
+    packet
+  })
+
+  /**
+   * 订阅消息
+   * @param topicObject 主题（可以是字符串、数组、对象）
+   * @param opts 配置项
+   * @param callback 回调函数
+   * @returns 客户端 client
+   */
+  client.subscribe(
+    ['msg1', 'msg2', 'msg3'],
+    {
+      qos: 0 // QoS 订阅级别：0-最多发一次、1-至少发一次、2-保证收一次（重要消息用 1 或 2）
+    },
+    (error, granted) => {
+      /**
+       * error：错误信息
+       * granted：订阅结果 { topic, qos }[]
+       */
+      if (error) {
+        console.log('订阅消息失败', {
+          time: new Date().toLocaleString(),
+          客户端id: client.options.clientId,
+          是否连接服务端: client.connected,
+          是否重连服务端: client.reconnecting,
+          client,
+          error,
+          granted
+        })
+        return
+      }
+      console.log('订阅消息成功', {
+        time: new Date().toLocaleString(),
+        客户端id: client.options.clientId,
+        是否连接服务端: client.connected,
+        是否重连服务端: client.reconnecting,
+        client,
+        error,
+        granted
+      })
+    }
+  )
+})
+
+// [事件]重新连接中
+client.on('reconnect', () => {
+  console.log('重新连接中', {
+    time: new Date().toLocaleString(),
+    客户端id: client.options.clientId,
+    是否连接服务端: client.connected,
+    是否重连服务端: client.reconnecting,
+    client
+  })
+})
+
+// [事件]连接失败
+client.on('error', error => {
+  /**
+   * error：错误信息
+   */
+  console.log('连接失败', {
+    time: new Date().toLocaleString(),
+    客户端id: client.options.clientId,
+    是否连接服务端: client.connected,
+    是否重连服务端: client.reconnecting,
+    client,
+    error
+  })
+})
+
+// [事件]连接关闭
+client.on('close', () => {
+  console.log('连接关闭', {
+    time: new Date().toLocaleString(),
+    客户端id: client.options.clientId,
+    是否连接服务端: client.connected,
+    是否重连服务端: client.reconnecting,
+    client
+  })
+})
+
+// [事件]收到消息
+client.on('message', (topic, payload, packet) => {
+  /**
+   * topic：消息主题
+   * payload：消息内容二进制，需要转换字符串
+   * packet：数据包
+   */
+  console.log('收到消息', {
+    time: new Date().toLocaleString(),
+    客户端id: client.options.clientId,
+    是否连接服务端: client.connected,
+    是否重连服务端: client.reconnecting,
+    client,
+    topic,
+    payload: payload.toString(),
+    packet
+  })
+})
+
+/**
+ * 发布消息
+ * @param topic 主题
+ * @param message 消息内容（可以是二进制）
+ * @param opts 配置项
+ * @param callback 回调函数
+ * @returns 客户端 client
+ */
+client.publish(
+  'msg1',
+  'hello mqtt',
+  {
+    qos: 0 // QoS 订阅级别：0-最多发一次、1-至少发一次、2-保证收一次（重要消息用 1 或 2）
+  },
+  (error, packet) => {
+    /**
+     * error：错误信息
+     * packet：数据包
+     */
+    if (error) {
+      console.log('发布消息失败', {
+        time: new Date().toLocaleString(),
+        客户端id: client.options.clientId,
+        是否连接服务端: client.connected,
+        是否重连服务端: client.reconnecting,
+        client,
+        error,
+        packet
+      })
+      return
+    }
+    console.log('发布消息成功', {
+      time: new Date().toLocaleString(),
+      客户端id: client.options.clientId,
+      是否连接服务端: client.connected,
+      是否重连服务端: client.reconnecting,
+      client,
+      error,
+      packet
+    })
+  }
+)
